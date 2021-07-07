abadf4edc82f2968c77b112f86ad5c127809aff6
Boris Fritscher <boris@fritscher.ch>
2021-07-07 17:49:06 +0200

    demo mounted event

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,5 +3,25 @@
     <h1>This is an about page</h1>
     <p>{{ $root.stateGlobal.uneValeur }}</p>
     <button v-on:click="$root.inc()">inc</button>
+    {{ result }}
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      result: [],
+    };
+  },
+  mounted() {
+    this.getApiData();
+  },
+  methods: {
+    getApiData() {
+      setTimeout(() => {
+        this.result = [1, 2, 3];
+      }, 1000);
+    },
+  },
+};
+</script>
