20d8cd47ffedde68cc8360cdb2035b2448ca9891
Boris Fritscher <boris@fritscher.ch>
2023-11-16 15:50:58 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <h1>Hello Bootstrap</h1>
+        <div class="row">
+            <div class="col-sm">
+                <button class="btn btn-primary">{{ message }}</button>
+            </div>
+            <div class="col-sm">
+                <i class="fas fa-ice-cream display-1 text-primary"></i>
+                <i class="fa-solid fa-car display-2 text-danger"></i>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello Vue!"
+        };
+    }
+};
+</script>
+
+<style></style>
