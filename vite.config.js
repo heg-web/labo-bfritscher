fdac4cf609809ead549fac22b7ae0ad4ed7121fd
Boris Fritscher <boris@fritscher.ch>
2024-05-02 09:22:52 +0200

    add gh-pages deploy with github actions

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    //eslint-disable-next-line no-undef
+    base: process.env.NODE_ENV === "production" ? "/labo-bfritscher/" : "/",
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url))
