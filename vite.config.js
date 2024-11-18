38296fa8cf25d0e918b24af9126a6bf0fe52b8be
Boris Fritscher <boris@fritscher.ch>
2024-11-18 14:06:41 +0100

    build and deploy on github

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
+    plugins: [vue(), vueDevTools()],
+    // eslint-disable-next-line
+    base: process.env.NODE_ENV === "production" ? "/labo-bfritscher/" : "/",
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url))
+        }
     }
-  }
 });
