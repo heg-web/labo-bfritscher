41217dd2c8085c8c6c2ceb4f297a99ac3cb6059d
Boris Fritscher <boris@fritscher.ch>
2025-05-20 10:57:11 +0200

    switch to webhashistory route rmode to make it work on refresh on github

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,12 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
+import Game from "../views/Game.vue";
+import Login from "../views/Login.vue";
+import Chat from "../views/Chat.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/",
@@ -14,6 +17,21 @@ const router = createRouter({
             path: "/about/:id",
             name: "about",
             component: AboutView
+        },
+        {
+            path: "/game",
+            name: "game",
+            component: Game
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: Login
+        },
+        {
+            path: "/chat",
+            name: "chat",
+            component: Chat
         }
     ]
 });
